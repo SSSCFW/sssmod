a51fd41e5368db1f7bd980e8bf5f851693bbd7c1
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2024-03-02 18:58:23 +0900

    add

--- src/main/java/com/example/examplemod/network/SetRapidfireNBT.java
+++ src/main/java/com/example/examplemod/network/SetRapidfireNBT.java
@@ -1,11 +1,8 @@
 package com.example.examplemod.network;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.network.CustomPayloadEvent;
 
 public class SetRapidfireNBT {
